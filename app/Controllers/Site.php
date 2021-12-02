2224df5ce21d6e0e00e133c131ab4cda69ab1b10
Gihovani Filipp Pereira Demetrio <gihovani@gmail.com>
2021-12-02 14:31:30 -0300

    ajustado menu home

--- app/Controllers/Site.php
+++ app/Controllers/Site.php
@@ -164,7 +164,7 @@ class Site extends BaseController
     {
         $config = new \App\Models\ConfiguracaoModel();
         $params['config'] = $config->first();
-        $params['version'] = '1.0.1';
+        $params['version'] = '1.0.2';
         return view('pages/' . $page, $params);
     }
 
